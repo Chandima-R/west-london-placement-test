c0a724f0b841cc6b7d4906bfb3c7c292a07743b9
chandima <chandimar.online@gmail.com>
2025-03-06 16:46:17 +0530

    fix: clean build issues

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,15 @@
 import type {Metadata} from "next";
+import {Arimo} from "next/font/google";
 import "../globals.css";
 import {Providers} from "@/modules/shared/provider";
 
+const arimo = Arimo({
+    variable: "--font-arimo-mono",
+    subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
-    title: "West London - IELTS | Signin",
+    title: "West London - IELTS | Dashboard",
     description: `Ace Your IELTS with Expert Guidance!
     Take the first step toward your foreign dream with West London IELTS Lab!
     Looking to achieve your dream IELTS score?
@@ -13,10 +19,14 @@ export const metadata: Metadata = {
     },
 };
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
     return (
         <html lang="en">
-        <body className={`antialiased`}>
+        <body className={`${arimo.variable} antialiased`}>
         <Providers>
             {children}
         </Providers>
